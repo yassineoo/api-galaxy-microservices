af2c5a2909610692f597025616991c1729138838
yassineoo <jy_attou@esi.dz>
2024-09-01 21:33:47 +0100

    fix auth

--- user-service/src/routes/apiRoute.ts
+++ user-service/src/routes/apiRoute.ts
@@ -21,20 +21,20 @@ import authenticate from "../services/middlewares/authenticate.middleware";
 const userApiRouter = Router();
 userApiRouter.get("/categories", getCategories);
 userApiRouter.get("/:userId", getAllApis);
+userApiRouter.get("/getReviews/:apiId", getAPIReview);
+userApiRouter.get("/adminReports", getReportsForAdmin);
+userApiRouter.get("/myApis/:userId", getUserApis);
+userApiRouter.get("/getRating/:apiId", getAPIRating);
 
+userApiRouter.get("/myFollowingApis/:userId", getUserFollowingsApis);
 userApiRouter.use(authenticate);
 
-userApiRouter.get("/reviewReports", getReviwesReports);
-userApiRouter.get("/adminReports", getReportsForAdmin);
 userApiRouter.get("/deleteReviewReport/:reportId", deleteReportReview);
 userApiRouter.post("/likeApi/:api_id", likeApi);
 userApiRouter.post("/dislikeApi/:api_id", dislikeApi);
-userApiRouter.get("/getReviews/:apiId", getAPIReview);
 userApiRouter.post("/createReview/:apiId", createReview);
-userApiRouter.get("/getRating/:apiId", getAPIRating);
 userApiRouter.get("/admin/:adminId", getAPISforAdmin);
-userApiRouter.get("/myApis/:userId", getUserApis);
-userApiRouter.get("/myFollowingApis/:userId", getUserFollowingsApis);
+
 userApiRouter.post("/reportAPI/:apiId", reportAnAPI);
 userApiRouter.post("/reportComment/:commentId", reportAnComment);
 
