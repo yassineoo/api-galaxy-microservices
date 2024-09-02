1774279bf599f2bc0310b1f5b0682bbd116fa88b
YASSINE ATTOU <jy_attou@esi.dz>
2024-09-02 20:44:23 +0100

    final payment (#13)
    Co-authored-by: Ayoub Abderrahmane Bousnane <bousnane45@gmail.com>

---
+++ payment-service/src/routes/stripeCrudRouter.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+import {
+  createCustomer,
+  getCustomerById,
+  updateCustomer,
+  deleteCustomer,
+  listCustomers,
+} from '../controllers/customerController';
+import {
+  createPrice,
+  getPrices,
+  getCustomerTransactionHistory,
+} from '../controllers/priceController';
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductWithPrice,
+} from '../controllers/productController';
+
+const stripeCrudRouter = express.Router();
+
+// Customer Routes
+stripeCrudRouter.post('/customers', createCustomer); // Create a new customer
+stripeCrudRouter.get('/customers', listCustomers); // List all customers
+stripeCrudRouter.get('/customers/:customerId', getCustomerById); // Retrieve a customer by ID
+stripeCrudRouter.put('/customers/:customerId', updateCustomer); // Update a customer's information
+stripeCrudRouter.delete('/customers/:customerId', deleteCustomer); // Delete a customer by ID
+
+// Customer Transaction History Route
+stripeCrudRouter.get('/customers/:customerId/transactions', getCustomerTransactionHistory); // Get customer transaction history
+
+// Product Routes
+stripeCrudRouter.get('/products', getProducts); // Get all products
+stripeCrudRouter.get('/products/:productId', getProductById); // Get a product by ID
+stripeCrudRouter.post('/products', createProduct); // Create a new product
+stripeCrudRouter.put('/products/:productId', updateProduct); // Update a product by ID
+stripeCrudRouter.delete('/products/:productId', deleteProduct); // Delete a product by ID
+stripeCrudRouter.post('/products/with-price', createProductWithPrice); // Create a new product with a price
+
+// Price Routes
+stripeCrudRouter.post('/prices', createPrice); // Create a new price
+stripeCrudRouter.get('/prices/:productId', getPrices); // Get prices of a specific product by product ID
+
+export default stripeCrudRouter;
